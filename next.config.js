37c31e1ff42a0826992f8d8908b3c40b21e91bf6
wholetone games <59148965+wholetonegames@users.noreply.github.com>
2023-05-01 13:34:27 +0100

    basepath

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: isProd ? '/website/' : '',
+  basePath: isProd ? '/website' : '',
   distDir: 'docs',
   images:{
     unoptimized: true,
