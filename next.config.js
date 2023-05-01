85e52220be8adf269e09e7c571f6816e955f49bd
wholetone games <59148965+wholetonegames@users.noreply.github.com>
2023-05-01 13:19:30 +0100

    prefix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
   distDir: 'docs',
-  output: 'export',
   images:{
     unoptimized: true,
-  }
+  },
+  reactStrictMode: true,
+  output: 'export',
 }
 
 module.exports = nextConfig
