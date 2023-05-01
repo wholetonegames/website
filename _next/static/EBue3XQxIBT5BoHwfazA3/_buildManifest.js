c14a4b312c423f7101ea674ecc1a16681ff65c45
wholetone games <59148965+wholetonegames@users.noreply.github.com>
2023-05-01 13:08:14 +0100

    Updates

--- _next/static/EBue3XQxIBT5BoHwfazA3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3f909ba5d96efd38.css","static/chunks/pages/index-1a8b2d2793e78446.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
